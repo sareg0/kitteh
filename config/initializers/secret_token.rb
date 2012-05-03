fc57916b4279536592cdf5b5efd15e86a8664703
Sara Regan <sararegan@Saras-MacBook-Pro.local>
2012-05-03 21:37:19 +0200

    My First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kitteh::Application.config.secret_token = 'ca9a605900cd6a655f734bea408841b0fd124ea4274f43549b4716d793ac82d2f48129dfbc5fab622b516af573e11fb44cfc19da1007e9693bb367f2a04377c0'
